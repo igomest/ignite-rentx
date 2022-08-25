30d50de5b1d00df2d4f8c3d6133e77995794e47b
Ivan Gomes <ivanjunior.pt@gmail.com>
2022-08-25 14:34:50 -0300

    implementing saving categories to the database and doing dependency injection

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -1,24 +1,20 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
 
 import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
 class CreateCategoryController {
-  constructor(private createCategoryRepositoryUseCase: CreateCategoryUseCase) {
-  }
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { name, description } = request.body;
 
-  handle(request: Request, response: Response): Response {
-    const {
-      name,
-      description,
-    } = request.body;
+    const createCategoryUseCase = container.resolve(CreateCategoryUseCase);
 
-    this.createCategoryRepositoryUseCase.execute({
+    await createCategoryUseCase.execute({
       name,
       description,
     });
 
-    return response.status(201)
-      .send();
+    return response.status(201).send();
   }
 }
 
