0620c2e85c1df6fb63dab52648cd18a3c8c750fd
Ivan Gomes <ivanjunior.pt@gmail.com>
2022-09-27 14:24:07 -0300

    Creating middleware of admin

--- src/shared/infra/typeorm/seed/admin.ts
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -7,14 +7,16 @@ import dataSource, { createConnection } from '..';
 async function create() {
   await createConnection('localhost');
 
-  const id = uuidV4;
+  const id = uuidV4();
   const password = await hash('admin', 8);
 
   await dataSource.query(
     `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, "driver_license")
-      values('${id}', 'admin', 'admin@rentx.com.br', ${password}, true, 'now()', 'XXXXX')
+      values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXX')
     `,
   );
+
+  await dataSource.destroy;
 }
 
 create().then(() => console.log('User admin created!'));
